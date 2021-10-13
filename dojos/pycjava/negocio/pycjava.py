5ba8b69fbe14d7cd194b0a201d5836c906495093
gvitor-s <gabrielvitorsales@gmail.com>
2021-10-13 20:31:44 -0300

    Dojo pycjava finish

--- dojos/pycjava/negocio/pycjava.py
+++ dojos/pycjava/negocio/pycjava.py
@@ -1,4 +1,16 @@
 def batalhar(escolha1, escolha2):
+    if escolha1 == escolha2:
+        return 'nenhum'
     if escolha1 == 'c':
-        return 'c'
-    return 'py'
+        if escolha2 == 'java':
+            return escolha2
+        return escolha1
+    if escolha1 == 'py':
+        if escolha2 == 'c':
+            return escolha2
+        return escolha1
+    if escolha1 == 'java':
+        if escolha2 == 'py':
+            return 'py'
+        return 'java'
+    return "ERROR"
--- dojos/pycjava/tests/test_pycjava.py
+++ dojos/pycjava/tests/test_pycjava.py
@@ -6,20 +6,88 @@ import negocio.pycjava as pycjava
 # Luigi
 # Luiz
 # Gabriel
+# Cezar
 
 class tests_pycjava(TestCase):
     def test_c_batalhando_contra_py_ganha_o_c(self):
         jogador1 = "c"
         jogador2 = "py"
+        esperado = 'c'
 
         vencedor = pycjava.batalhar(jogador1, jogador2)
 
-        self.assertEqual(jogador1, vencedor)
-    
-    def test_j_batalhando_contra_py_ganha_py(self):
+        self.assertEqual(esperado, vencedor)
+
+    def test_c_batalhando_contra_java_ganha_java(self):
+
+        jogador1 = 'c'
+        jogador2 = 'java'
+        esperado = 'java'
+
+        vencedor = pycjava.batalhar(jogador1, jogador2)
+
+        self.assertEqual(esperado, vencedor)
+
+    def test_c_batalhando_contra_c_empate(self):
+        jogador1 = 'c'
+        jogador2 = 'c'
+        esperado = 'nenhum'
+
+        vencedor = pycjava.batalhar(jogador1, jogador2)
+
+        self.assertEqual(esperado, vencedor)
+
+    def test_py_batalhando_contra_c_ganha_c(self):
+        jogador1 = 'py'
+        jogador2 = 'c'
+        esperado = 'c'
+
+        vencedor = pycjava.batalhar(jogador1, jogador2)
+
+        self.assertEqual(esperado, vencedor)
+
+
+    def test_java_batalhando_contra_py_ganha_py(self):
         jogador1 = 'java'
         jogador2 = 'py'
+        esperado = 'py'
 
         vencedor = pycjava.batalhar(jogador1, jogador2)
 
-        self.assertEqual(jogador2, vencedor)
+        self.assertEqual(esperado, vencedor)
+
+    def test_java_batalhando_contra_c_ganha_java(self):
+        jogador1 = 'java'
+        jogador2 = 'c'
+        esperado = 'java'
+
+        vencedor = pycjava.batalhar(jogador1, jogador2)
+
+        self.assertEqual(esperado, vencedor)
+
+    def test_py_batalhando_contra_java_ganha_py(self):
+        jogador1 = 'py'
+        jogador2 = 'java'
+        esperado = 'py'
+
+        vencedor = pycjava.batalhar(jogador1, jogador2)
+
+        self.assertEqual(esperado, vencedor)
+
+    def test_invalido_contra_java_retorna_erro(self):
+        jogador1 = 'ruby'
+        jogador2 = 'java'
+        esperado = 'ERROR'
+
+        vencedor = pycjava.batalhar(jogador1, jogador2)
+
+        self.assertEqual(esperado, vencedor)
+        
+    def test_C_batalhando_contra_Java_retorna_ERROR(self):
+        jogador1 = 'C'
+        jogador2 = 'Java'
+        esperado = 'ERROR'
+
+        vencedor = pycjava.batalhar(jogador1, jogador2)
+
+        self.assertEqual(esperado, vencedor)
