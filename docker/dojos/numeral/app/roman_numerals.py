920502ee5682f2faedc098ce853f173118a0ae40
cpereira42 <cpereira@42sp.org.br>
2021-10-18 10:16:38 -0300

    to romam done

--- docker/dojos/numeral/app/roman_numerals.py
+++ docker/dojos/numeral/app/roman_numerals.py
@@ -1,42 +1,24 @@
 def to_roman(decimal_number):
-    tmp = decimal_number
-    count = len(str(decimal_number))
-
-    
-
     m = decimal_number//1000
     decimal_number %= 1000
-    d = decimal_number // 500
-    decimal_number %= 500
     c = decimal_number//100
     decimal_number %= 100
-    l = decimal_number // 50
-    decimal_number %= 50
     x = decimal_number//10
     decimal_number %= 10
-    v = decimal_number // 5
-    decimal_number %= 5
     i = decimal_number
 
-    if decimal_number == 5:
-        return 'V'
-    elif decimal_number == 10:
-        return 'X'
-    elif decimal_number == 50:
-        return 'L'
-    elif decimal_number == 100:
-        return 'C'
-    elif decimal_number == 500:
-        return 'D'
-    elif decimal_number == 1000:
-        return 'M'
-    elif decimal_number == 2:
-        return 'II'
-    elif decimal_number == 3:
-        return 'III'
-    elif decimal_number == 4:
-        return 'IV'
-    return 'I'
+    if(m <= 3):
+        strin = printer(m, 'M',' ',' ')
+        strin +=printer(c, 'C','D','M')
+        strin +=printer(x, 'X','L','C')
+        strin +=printer(i, 'I','V','X')
+        print(strin)
+    else:
+        print("invalid convert")
+
+def from_roman2(roman_numerl):
+
+    return
 
 def from_roman(roman_number):
     if roman_number == 'V':
@@ -52,3 +34,31 @@ def from_roman(roman_number):
     if roman_number == 'M':
         return 1000
     return 1
+
+def printer(number,r, n, p):
+    if (number == 1):
+        return(r)
+    elif(number == 2):
+        return ("%c%c" % (r, r))
+    elif(number == 3):
+        return ("%c%c%c" % (r, r, r))
+    elif(number == 4):
+        return ("%c%c" % (r, n))
+    elif(number == 5):
+        return ("%c" % n),
+    elif(number == 6):
+        return ("%c%c" % (n, r))
+    elif(number == 7):
+        return ("%c%c%c" % (n, r, r))
+    elif(number == 8):
+        return ("%c%c%c%c" % (n, r, r, r))
+    if (number == 9):
+        return ("%c%c" % (r, p))
+    return 
+
+
+if (__name__ == "__main__"):
+    print("Numeral romano")
+    to_roman(1494)
+
+
