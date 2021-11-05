a08795abb3cd56160d3a8fa3ccc92630a6edc73d
gvitor-s <gvitor-s@student.42sp.org.br>
2021-11-05 08:03:32 -0300

    feat: add function to search in database

---
+++ Requisicao_BD/requisicao.py
@@ -0,0 +1,39 @@
+import pymysql
+import pandas
+import os
+
+def connect_db():
+	try:
+		my_db = pymysql.connect(
+                host=os.getenv("HOSTNAME"),
+                user=os.getenv("USER_DB"),
+                password=os.getenv("PASSWD"),
+                database="decasoft_xavier"
+			)
+		return my_db
+
+	except:
+		print("Informações erradas")
+
+def save_to_csv(query, con, file):
+	results = pandas.read_sql_query(query, con)
+	results.to_csv(file, index=False)
+
+if (__name__ == "__main__"):
+	con = connect_db()
+
+	out_csv = "out.csv"
+	query = "SELECT nome FROM empresas WHERE 1"
+	save_to_csv(query, con, out_csv)
+
+	cursor = con.cursor()
+
+	df = pandas.read_csv(out_csv)
+	#df.to_sql("logs", con, if_exists='append', index=False)
+
+	version="0.0.1"
+	result_csv = str(df.to_csv(header=False, index=False)).replace('\'', '')
+	cursor.execute(f"INSERT INTO logs (query, version, result) VALUES ('{query}', '{version}', '{result_csv}')")
+	con.commit()
+
+	con.close()
