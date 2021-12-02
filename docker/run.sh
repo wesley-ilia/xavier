10677aa47602c45245e46d4b0b00ada21b8ef40f
LuigiFerrari <luigienzopereiraferrari@gmail.com>
2021-12-02 13:28:33 -0300

    remove dojo training

--- docker/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Create a docker image
-docker build -t image_labs .
-
-# Remove container container_42api
-docker rm container_labs
-
-# Run docker
-docker run --name container_labs -it image_labs
