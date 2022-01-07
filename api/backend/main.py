41f605920bafe3015dc86fa68b6c37b15a3bdf40
Luiz Felipe <lfelipecarvalhedo@gmail.com>
2022-01-07 18:12:43 -0300

    added merge to the information uploaded by the user

--- api/backend/main.py
+++ api/backend/main.py
@@ -8,8 +8,7 @@ from Log import Log
 import pandas as pd
 import numpy as np
 import os
-
-
+from unidecode import unidecode
 
 todas_capitais = ["Rio Branco", "Maceió", "Macapá", "Manaus", "Salvador", "Fortaleza", "Brasília", "Vitória", "Goiânia", "São Luís", "Cuiabá", "Campo Grande", "Belo Horizonte", "Belém", "João Pessoa", "Curitiba", "Recife", "Teresina", "Rio de Janeiro", "Natal", "Porto Alegre", "Porto Velho", "Boa Vista", "Florianópolis", "São Paulo", "Aracaju", "Palmas"]
 
@@ -31,14 +30,29 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+def agg(a):
+    a = a.replace(np.nan, "")
+    # a.dropna(inplace=True)
+    a.reset_index(drop=True, inplace=True)
+    ret = ", ".join(set(", ".join(a).split(', ')))
+    return ret
+
 def initialize():
+    global df
     log = Log()
     global db
     db = pd.read_sql_query("SELECT * FROM empresa_completa3 WHERE 1", log.con)
+    df = db
 
     db['mercado'].replace(np.nan, "", inplace=True)
-    mercados = [x for x in list(set(db['mercado'].to_list())) if x]
+    total_mercados = db['mercado'].to_list()
+    mercados = []
+    for mercado in total_mercados:
+        if mercado:
+            mercados.extend([s.strip() for s in mercado.split(',')])
+    mercados = [x for x in list(set(mercados)) if x]
     mercados.sort()
+
     db['stacks'].replace(np.nan, "", inplace=True)
     total_stacks = db['stacks'].to_list()
     stacks = []
@@ -50,6 +64,7 @@ def initialize():
 
     global dropdown_list
     dropdown_list = {"mercados": mercados, "stacks": stacks}
+    print("mercado = " + db['mercado'][6498])
 
 initialize()
 
@@ -57,22 +72,66 @@ def importFile (NomeArquivo):
     arquivo = pd.read_csv(NomeArquivo)
     if 'nome' not in arquivo.columns:
         print("veio errado")
+    
+    arquivo.replace(r'^\s*$', np.NaN, regex=True, inplace=True)
 
+    erradas = arquivo.loc[arquivo['nome'].isnull()]
+    adicionar = arquivo.loc[arquivo['nome'].notnull()]
+
+    adicionar['nome'] = adicionar['nome'].str.strip()
+    aggregation_functions = {'estado': 'first', 'cidade': 'first', 'mercado': agg}
+    adicionar = adicionar.groupby(['nome'], as_index=False).aggregate(aggregation_functions)
+
+    adicionar.reset_index(drop=True, inplace=True)
+    
     columns = ['estado', 'cidade', 'mercado', 'stacks']
-    for i in range(len(arquivo.index)):
-        if arquivo['nome'].isna:
-            print("veio errado")
-        index = db.index[db['nome'] == arquivo['nome'][i]].tolist()
+
+    print(db)
+
+    df = pd.concat([db, adicionar], axis=0, ignore_index=True)
+    print("mercado = " + df['mercado'][6498])
+    aggregation_functions = {'estado': 'first', 'cidade': 'first', 'mercado': agg}
+    df = df.groupby(['nome'], as_index=False).aggregate(aggregation_functions)
+    df.reset_index(drop=True, inplace=True)
+    print(df)
+
+    """ for i in adicionar.index:
+        index = db.index[db['nome'] == adicionar['nome'][i]].tolist()
+        print(index)
         if index:
             index = index[0]
             for col in columns:
-                if col in arquivo.columns:
-                    print(db[col][index])
-                    print(arquivo[col][i])
-                    arquivo[col] = ", ".join(set((db[col][index] + " " + arquivo[col][i]).split()))
-            
-    print(arquivo)
+                if col in adicionar.columns and col not in ['estado', 'cidade']:
+                    temp = unidecode(adicionar[col][i].strip().lower())
+                    print("temp = ")
+                    print(temp)
+                    temp = ' '.join(temp.split())
+                    print(temp)
+                    adicionar[col][i] = ", ".join(set((db[col][index] + ", " + temp).split(", "))).strip() """
+
+    for col in columns:
+        if col not in adicionar.columns:
+            adicionar[col] = np.full(len(adicionar.index), np.nan)
+    
+    adicionar.replace(np.nan, "", inplace=True)
+
+    adicionar_list = adicionar[['nome', 'estado', 'cidade', 'mercado', 'stacks']].values.tolist()
+
+    print(adicionar_list)
+
+    log = Log()
+    df = pd.read_sql_query("SELECT * FROM tb_usuario WHERE 1", log.con)
+    for adc in adicionar_list:
+        if adc[0] in df['nome']:
+            stmt = f"UPDATE tb_usuario (`estado`, `cidade`, `mercado`, `stacks`) VALUES (%s, %s, %s, %s), WHERE nome={adc[0]}"
+            log.cursor.execute(stmt, adicionar_list)
+    stmt = "INSERT INTO tb_usuario (`nome`, `estado`, `cidade`, `mercado`, `stacks`) VALUES (%s, %s, %s, %s, %s)"
+    log.cursor.executemany(stmt, adicionar_list)
+    log.con.commit()
+
+    print(adicionar)
     os.remove(NomeArquivo)
+    initialize()
 
 
 @app.post("/api/uploadfile")
@@ -89,6 +148,7 @@ def dropdown():
 @app.get("/search")
 def get_info(market: str, stack: str, state: str, capitais: str, cidade: str = "", file_name: str='Untitled',
              extension: str = "csv"):
+    
     if capitais == "sim":
         cidade = ""
     elif capitais == "nao":
@@ -108,8 +168,12 @@ def get_info(market: str, stack: str, state: str, capitais: str, cidade: str = "
         query += state_query
         if market:
             query += ' and '
-            market_query = "(mercado=='" + market.replace(',', "' or mercado=='") + "')"
-            query += market_query
+            markets = market.split(',')
+            print(markets)
+            for i in range(len(markets)):
+                query += f'mercado.str.contains("{markets[i]}", na=False).values'
+                if i < len(markets) - 1:
+                    query += ' or '
         if stack:
             query += ' and '
             stacks = stack.split(',')
@@ -126,8 +190,12 @@ def get_info(market: str, stack: str, state: str, capitais: str, cidade: str = "
                 print(cidade_query)
                 query += cidade_query
     elif market:
-        market_query = "(mercado=='" + market.replace(',', "' or mercado=='") + "')"
-        query += market_query
+        markets = market.split(',')
+        for i in range(len(markets)):
+            query += f'mercado.str.contains("{markets[i]}", na=False).values'
+            if i < len(markets) - 1:
+                query += ' or '
+
         if stack:
             query += ' and '
             stacks = stack.split(',')
@@ -135,6 +203,7 @@ def get_info(market: str, stack: str, state: str, capitais: str, cidade: str = "
                 query += f'stacks.str.contains("{stacks[i]}", na=False).values'
                 if i < len(stacks) - 1:
                     query += ' or '
+
     elif stack:
         stacks = stack.split(',')
         for i in range(len(stacks)):
@@ -178,10 +247,16 @@ def get_preview(state: str, cidade: str, market:str, stack: str, capitais: str):
     if state and state != 'TODOS':
         state_query = "(estado==' " + state.replace(',', "' or estado==' ") + "')"
         query += state_query
+
         if market:
             query += ' and '
-            market_query = "(mercado=='" + market.replace(',', "' or mercado=='") + "')"
-            query += market_query
+            markets = market.split(',')
+            print(markets)
+            for i in range(len(markets)):
+                query += f'mercado.str.contains("{markets[i]}", na=False).values'
+                if i < len(markets) - 1:
+                    query += ' or '
+
         if stack:
             query += ' and '
             stacks = stack.split(',')
@@ -197,9 +272,14 @@ def get_preview(state: str, cidade: str, market:str, stack: str, capitais: str):
                 cidade_query = "(cidade=='" + cidade.replace(',', "' or cidade=='") + "')"
                 print(cidade_query)
                 query += cidade_query
+
     elif market:
-        market_query = "(mercado=='" + market.replace(',', "' or mercado=='") + "')"
-        query += market_query
+        markets = market.split(',')
+        for i in range(len(markets)):
+            query += f'mercado.str.contains("{markets[i]}", na=False).values'
+            if i < len(markets) - 1:
+                query += ' or '
+
         if stack:
             query += ' and '
             stacks = stack.split(',')
@@ -207,6 +287,7 @@ def get_preview(state: str, cidade: str, market:str, stack: str, capitais: str):
                 query += f'stacks.str.contains("{stacks[i]}", na=False).values'
                 if i < len(stacks) - 1:
                     query += ' or '
+
     elif stack:
         stacks = stack.split(',')
         for i in range(len(stacks)):
