0a9207f8a9e75a9c5184081f2cf889757f702364
gvitor-s <gvitor-s@student.42sp.org.com>
2022-02-11 13:30:54 -0300

    fix: put on pep8 backend main

--- api/backend/main.py
+++ api/backend/main.py
@@ -3,17 +3,30 @@ import shutil
 # from fastapi.params import Query
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
-from dotenv import load_dotenv
 import pandas as pd
 import numpy as np
 from os import getenv, remove
 from unidecode import unidecode
 from sqlalchemy import create_engine
-import time
-from io import StringIO
+# import time
 from utils import save_pdf
 
-todas_capitais = ["rio branco", "maceio", "macapa", "manaus", "salvador", "fortaleza", "brasilia", "vitoria", "goiania", "sao luis", "cuiaba", "campo grande", "belo horizonte", "belem", "joao pessoa", "curitiba", "recife", "teresina", "rio de janeiro", "natal", "porto alegre", "porto velho", "boa vista", "florianopolis", "sao paulo", "aracaju", "palmas"]
+todas_capitais = [
+        "rio branco", "maceio",
+        "macapa", "manaus",
+        "salvador", "fortaleza",
+        "brasilia", "vitoria",
+        "goiania", "sao luis",
+        "cuiaba", "campo grande",
+        "belo horizonte", "belem",
+        "joao pessoa", "curitiba",
+        "recife", "teresina",
+        "rio de janeiro",
+        "natal", "porto alegre",
+        "porto velho", "boa vista",
+        "florianopolis", "sao paulo",
+        "aracaju", "palmas"
+        ]
 
 origins = [
     "http://localhost:3000",
@@ -39,6 +52,7 @@ engine = create_engine(f'postgresql://{user}:{passwd}\
 
 db = pd.read_sql_table("main", engine)
 
+
 def agg(a):
     a = a.replace(np.nan, "")
     # a.dropna(inplace=True)
@@ -47,6 +61,7 @@ def agg(a):
     ret = ", ".join(set(s))
     ret = ret.strip()
     return ret
+
 
 def initialize():
     db['mercado'].replace(np.nan, "", inplace=True)
@@ -68,20 +83,31 @@ def initialize():
     stacks.sort()
 
     global dropdown_list
-    dropdown_list = {"mercados": mercados, "stacks": stacks, "colunas": db.columns.tolist()}
+    dropdown_list = {
+            "mercados": mercados,
+            "stacks": stacks,
+            "colunas": db.columns.tolist()
+            }
+
 
 initialize()
 
-def update_db(adicionar):
-    t0 = time.time()
 
-    aggregation_db = {'estado': 'first', 'cidade': 'first', 'mercado': agg, 'stacks': agg}
+def update_db(adicionar):
+    # t0 = time.time()
+
+    aggregation_db = {
+            'estado': 'first',
+            'cidade': 'first',
+            'mercado': agg,
+            'stacks': agg
+            }
     for col in db.columns:
         if col not in aggregation_db.keys():
             aggregation_db[col] = 'first'
-    
     adicionar = pd.concat([db, adicionar], axis=0, ignore_index=True)
-    adicionar = adicionar.groupby(['nome'], as_index=False).aggregate(aggregation_db)
+    adicionar = adicionar.groupby(
+            ['nome'], as_index=False).aggregate(aggregation_db)
     adicionar.reset_index(drop=True, inplace=True)
 
     adicionar['id'] = adicionar.index
@@ -92,21 +118,20 @@ def update_db(adicionar):
     adicionar.to_sql("main", engine, if_exists='replace', index=False)
     """ with engine.connect() as con:
         con.execute('ALTER TABLE `empresa_merge_teste` ADD PRIMARY KEY (id);') """
-    t1 = time.time()
-    total = t1-t0
+    # t1 = time.time()
+    # total = t1-t0
     # print("total time 2 = " + str(total))
     return adicionar
 
-def importFile (NomeArquivo):
+
+def importFile(NomeArquivo):
     arquivo = pd.read_csv(NomeArquivo)
     if 'nome' not in arquivo.columns:
         return False, ""
-    
     arquivo.replace(r'^\s*$', np.NaN, regex=True, inplace=True)
 
     erradas = arquivo.loc[arquivo['nome'].isnull()]
     adicionar = arquivo.loc[arquivo['nome'].notnull()]
-
 
     columns = ['estado', 'cidade', 'mercado', 'stacks']
     for col in columns:
@@ -114,12 +139,17 @@ def importFile (NomeArquivo):
             adicionar[col] = np.full(len(adicionar.index), np.nan)
 
     adicionar['nome'] = adicionar['nome'].str.strip().str.lower()
-    aggregation_user = {'estado': 'first', 'cidade': 'first', 'mercado': agg, 'stacks': agg}
-    
-    t0 = time.time()
+    aggregation_user = {
+            'estado': 'first',
+            'cidade': 'first',
+            'mercado': agg,
+            'stacks': agg
+            }
+    # t0 = time.time()
     df = pd.read_sql_table("user", engine)
     adicionar = pd.concat([df, adicionar], axis=0, ignore_index=True)
-    adicionar = adicionar.groupby(['nome'], as_index=False).aggregate(aggregation_user)
+    adicionar = adicionar.groupby(
+            ['nome'], as_index=False).aggregate(aggregation_user)
 
     adicionar.reset_index(drop=True, inplace=True)
 
@@ -131,32 +161,31 @@ def importFile (NomeArquivo):
     adicionar.to_sql("user", engine, if_exists='replace', index=False)
     """ with engine.connect() as con:
         con.execute('ALTER TABLE `user` ADD PRIMARY KEY (id);') """
-    t1 = time.time()
-    total = t1-t0
+    # t1 = time.time()
+    # total = t1-t0
     # print("total time 1 = " + str(total))
 
     global db
     db = update_db(adicionar)
-    
     remove(NomeArquivo)
     initialize()
     return True, erradas
 
-def build_query(state: str, cidade: str, market:str, stack: str, capitais: str):
+
+def build_query(state: str, cidade: str, market: str, stack: str, capitais: str):
     state = state.lower()
-    
     if capitais == "sim":
         cidade = ""
     elif capitais == "nao":
         capitais_juntas = "' and cidade!='".join(todas_capitais)
         cidade = "(cidade!='" + capitais_juntas + "')"
-    
     stack = stack.replace("cpp","c\+\+")
     stack = stack.replace("csharp","c#")
 
     query = ""
     if state and state != 'TODOS':
-        state_query = "(estado=='" + state.replace(',', "' or estado=='") + "')"
+        state_query = "(estado=='" + state.replace(
+            ',', "' or estado=='") + "')"
         query += state_query
         if market:
             query += ' and ('
@@ -179,7 +208,8 @@ def build_query(state: str, cidade: str, market:str, stack: str, capitais: str):
             if capitais == 'nao':
                 query += cidade
             else:
-                cidade_query = "(cidade=='" + cidade.replace(',', "' or cidade=='") + "')"
+                cidade_query = "(cidade=='" + cidade.replace(
+                    ',', "' or cidade=='") + "')"
                 query += cidade_query
     elif market:
         query += '('
@@ -215,7 +245,6 @@ def build_query(state: str, cidade: str, market:str, stack: str, capitais: str):
                 query += cidade
     else:
         return '0'
-    
     return query
 
 # @app.get('/api/get_env')
@@ -230,19 +259,21 @@ def build_query(state: str, cidade: str, market:str, stack: str, capitais: str):
 # 		'measurementId': getenv('MEASUREMENTID')
 # 	}
 
+
 @app.post("/api/uploadfile")
-async def upload(file: UploadFile=File(...)):
-    with open(f"{file.filename}","wb") as buffer:
+async def upload(file: UploadFile = File(...)):
+    with open(f"{file.filename}", "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
     worked, erradas = importFile(file.filename)
     if worked:
         erradas = ", ".join([str(i) for i in (erradas.index + 1)])
         if erradas:
             # print(erradas)
-            return {"message" : "As linhas " + erradas + " são inválidas, as demais foram adicionadas com sucesso!!"}
-        return {"message" : 'Cadastrados com sucesso'}
+            return {"message": "As linhas " + erradas + " são inválidas, as demais foram adicionadas com sucesso!!"}
+        return {"message": 'Cadastrados com sucesso'}
     else:
-        return {"message" : "Coluna 'nome' está faltando"}
+        return {"message": "Coluna 'nome' está faltando"}
+
 
 @app.get("/api/download-user-table")
 def user_table():
@@ -250,13 +281,16 @@ def user_table():
     df.to_csv('tabela_usuario.csv', sep=',', index=False)
     return FileResponse('tabela_usuario.csv', filename='tabela_usuario.csv')
 
+
 @app.get("/dropdown")
 def dropdown():
     return dropdown_list
 
+
 @app.get("/search")
-def get_info(market: str, stack: str, state: str, capitais: str, colunas: str, 
-             cidade: str = "", file_name: str='Untitled', extension: str = "csv"):
+def get_info(market: str, stack: str, state: str, capitais: str, colunas: str,
+             cidade: str = "", file_name: str = 'Untitled',
+             extension: str = "csv"):
     if not file_name:
         file_name = 'Untitled'
     file_name += '.' + extension
@@ -279,6 +313,7 @@ def get_info(market: str, stack: str, state: str, capitais: str, colunas: str,
         save_pdf(df, file_name)
     return FileResponse(file_name, filename=file_name)
 
+
 @app.get("/cidades")
 def get_cidades(state: str):
     state = state.lower()
@@ -290,12 +325,13 @@ def get_cidades(state: str):
     cididades.sort()
     return (cididades)
 
+
 @app.get("/preview")
-def get_preview(state: str, cidade: str, market:str, stack: str, capitais: str):
+def get_preview(
+        state: str, cidade: str, market: str, stack: str, capitais: str):
     query = build_query(state, cidade, market, stack, capitais)
     if query == '0':
         return '0'
     print(query)
-    
     df = db.query(query)
     return len(df.index)
