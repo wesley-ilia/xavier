73243829b73003acfba3435757f38637f383baed
Luiz Felipe <lfelipecarvalhedo@gmail.com>
2022-02-14 15:58:31 -0300

    add: google maps api search

--- search_location.py
+++ search_location.py
@@ -28,6 +28,34 @@ def search_address(empresa: str):
 	response = requests.request("GET", url, headers=headers, data=payload)
 	return response.json()
 
+def search_autocomplete(empresa: str):
+	url = ("https://maps.googleapis.com/maps/api/place/autocomplete/json?" +
+	f"input={empresa}&types=establishment&key={apiKey}")
+
+	payload={}
+	headers = {}
+
+	response = requests.request("GET", url, headers=headers, data=payload)
+	return response.json()
+
+def search_text(empresa: str):
+	url = ("https://maps.googleapis.com/maps/api/place/textsearch/json?" +
+	f'query="{empresa}"&key={apiKey}')
+
+	payload={}
+	headers = {}
+
+	response = requests.request("GET", url, headers=headers, data=payload)
+	return response.json()
+
+def get_city_state2(description: str):
+	address_list = description.split(',')
+	print(address_list)
+	city_state = address_list[-2].strip().split('-')
+	city = city_state[0].strip()
+	state = city_state[1].replace('State of', '').strip()
+	return city, state
+
 def get_city_state(formatted_address: str):
 	address_list = formatted_address.split(',')
 	city_state = address_list[-3].strip().split('-')
@@ -35,15 +63,38 @@ def get_city_state(formatted_address: str):
 	state = city_state[1].strip()
 	return city, state
 
-for i, name in enumerate(db['nome']):
-	response = search_address(name + ' Brasil')
-	city, state = 'nada', 'nada'
-	if response['status'] == 'OK':
-		city, state = get_city_state(response['candidates'][0]['formatted_address'])
-	if not pd.isna(db['cidade'][i]):
-		print('---------------------------')
-		print('nome = ', name)
-		print('cidade achada = ', city)
-		print('cidade do banco = ', db['cidade'][i])
-	if i == 20:
-		break
+# counter = 0
+# for i, nome in enumerate(db['nome']):
+# 	response = search_text(nome + ' in Brasil')
+# 	city, state = 'nada', 'nada'
+	
+# 	if response['status'] == 'OK':
+# 		city, state = get_city_state(response['results'][0]['formatted_address'])
+# 	if not pd.isna(db['cidade'][i]):
+# 		print('---------------------------')
+# 		print('nome = ', db['nome'][i])
+# 		print('cidade achada = ', city)
+# 		print('cidade do banco = ', db['cidade'][i])
+# 		if db['cidade'][i]== city.lower():
+# 			counter += 1
+# 	if i == 20:
+# 		break
+# print('acertos ', counter)
+
+
+counter = 0
+for i, nome in enumerate(db['nome']):
+	if pd.isna(db['estado'][i]):
+		print('i =', i)
+		response = search_text(nome + ' in Brasil')
+		
+		if response['status'] == 'OK':
+			city, state = get_city_state(response['results'][0]['formatted_address'])
+			counter += 1
+			db['estado'][i] = state
+			db['cidade'][i] = city
+
+		if counter == 20:
+			break
+
+db.to_sql("main", engine, if_exists='replace', index=False)
