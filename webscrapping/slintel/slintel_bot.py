c8e403e7c671d45d56d0c4e20f2ec0987bf1d6e4
gvitor-s <gvitor-s@student.42sp.org.com>
2022-02-16 14:28:11 -0300

    solve merge with refactory_slintel branch

--- webscrapping/slintel/slintel_bot.py
+++ webscrapping/slintel/slintel_bot.py
@@ -1,27 +1,37 @@
+from os import environ
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from os import environ
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 class Slintel(webdriver.Chrome):
     def __init__(
         self,
-        teardown: bool = False,
-        implicit_wait: int = 0,
         driver_path: str = './',
         headless: bool = False
             ) -> None:
 
-        self.teardown = teardown
         environ['PATH'] += driver_path
         chrome_options = Options()
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument("--disable-dev-shm-usage")
+        capabilities = DesiredCapabilities().CHROME
+        if headless is True:
+            capabilities.update(pageLoadStrategy="eager")
+            chrome_options.add_argument('--headless')
+            chrome_options.add_argument('--no-sandbox')
+            chrome_options.add_argument('--disable-gpu')
+            chrome_options.add_argument('--max-size=1920,1080')
+            chrome_options.add_argument("--disable-dev-shm-usage")
         super(Slintel, self).__init__(
+            desired_capabilities=capabilities,
             options=chrome_options
         )
-        self.implicitly_wait(implicit_wait)
+        self.implicitly_wait(4)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, tp, vl, tb):
+        self.close()
 
     def land_in_page(self, page: str = None):
         self.get(page)
---
+++ webscrapping/slintel/utils_get_by_website.py
@@ -0,0 +1,135 @@
+from s3 import S3
+from os import getenv
+from time import sleep
+from bs4 import BeautifulSoup
+import concurrent.futures as cf
+from slintel_bot import Slintel
+from pandas import read_parquet, DataFrame
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+
+BASE_URL = "https://www.slintel.com/search?searchTerm="
+
+stacks_box_needed = {
+        "Programming Languages And Frameworks",
+        "Devops And Development",
+        "IT Security",
+        "IT Management",
+        "Platform And Storage",
+        "Computer Networks",
+        "Operations Software",
+        "Testing And QA"
+        }
+
+
+def loading():
+    with cf.ProcessPoolExecutor() as worker:
+        data = worker.map(packaging, loading_info_from_startupbase_parquet())
+    return {name: stacks for name, stacks in data}
+
+
+def delivery(data):
+    df = DataFrame(
+            data={
+                "name": data.keys(),
+                "stacks": data.values()
+                }
+            )
+    driver = S3()
+    driver.send_to_s3(
+            df=df,
+            bucket_name=getenv("BUCKET_NAME"),
+            destination=getenv("DESTINATION")
+            )
+
+
+def format_website(website):
+    website = website.replace("https:", '').replace("http:", '')
+    website = website.replace("www.", '').replace("//", '').replace('/', '')
+    return website
+
+
+def format_names(name):
+    name = name.replace(".com", '').replace('[', '')
+    name = name.replace('[', '').replace(':', '')
+    return name
+
+
+def select_itens_in_dropbox(bot: Slintel):
+    """Select which itens of stacks_box_needed
+    has in dropdown of company page"""
+    try:
+        dropdown = bot.find_element(by=By.CLASS_NAME, value="techstack_select")
+    except NoSuchElementException:
+        return ()
+    return filter(
+            lambda x: x.text in stacks_box_needed,
+            dropdown.find_elements(
+                by=By.TAG_NAME, value="option")
+            )
+
+
+def get_stacks_from_page(bot: Slintel, selector: str) -> set:
+    """Parse stacks from html page"""
+    stacks = bot.find_element(
+            by=By.CSS_SELECTOR,
+            value=selector
+            )
+    soup = BeautifulSoup(stacks.get_attribute('innerHTML'), 'html.parser')
+    return {stack.text.strip(" \n") for stack in soup.select('a')}
+
+
+def get_first_element_name_on_dropdown(bot: Slintel):
+    try:
+        first_element_in_dropdown = bot.find_element(
+            by=By.CSS_SELECTOR,
+            value='select.techstack_select > option'
+            ).text
+    except NoSuchElementException:
+        return None
+    return first_element_in_dropdown
+
+
+def loading_info_from_startupbase_parquet():
+    with S3() as connection:
+        name = getenv("FILE_NAME")
+        connection.download_from_s3(
+            bucket_name=getenv('BUCKET_NAME'),
+            obj_name=getenv("OBJ_NAME"),
+            name=name
+            )
+        sleep(1)
+        df_startup = read_parquet(name)
+        df_startup["website"] = df_startup["website"].str.lower()
+        return zip(
+            map(format_website, df_startup['website']),
+            map(format_names, df_startup['name'])
+            )
+
+
+def packaging(values: tuple) -> dict:
+    name = values[1]
+    stacks = set()
+    with Slintel(headless=True) as bot:
+        bot.land_in_page(BASE_URL + values[0])
+        try:
+            button = bot.find_element(
+                    by=By.CSS_SELECTOR, value="div.media-body > h5 > a")
+            button.click()
+        except NoSuchElementException:
+            return (name, stacks)
+        itens_in_dropbox = select_itens_in_dropbox(bot)
+        if get_first_element_name_on_dropdown(bot) in stacks_box_needed:
+            stacks.update(get_stacks_from_page(
+                bot,
+                'div[class="row techstack_card show_techstack_init"]'
+                ))
+            next(itens_in_dropbox)
+        for item in itens_in_dropbox:
+            item.click()
+            stacks.update(get_stacks_from_page(
+                bot,
+                'div[class="row techstack_card show_techstack_onchange"]'
+                ))
+    return (name, stacks)
