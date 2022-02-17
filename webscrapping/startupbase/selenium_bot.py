95861f1e1f8b45ee4e139a5252c9247b082fc7ae
gvitor-s <gvitor-s@student.42sp.org.com>
2022-02-17 14:06:24 -0300

    feat: disable share memory on selenium startupbase

--- webscrapping/startupbase/selenium_bot.py
+++ webscrapping/startupbase/selenium_bot.py
@@ -15,6 +15,7 @@ class StartupBase(webdriver.Chrome):
             chrome_options.add_argument('--no-sandbox')
             chrome_options.add_argument('--disable-gpu')
             chrome_options.add_argument('--disable-extensions')
+            chrome_options.add_argument('--disable-dev-shm-usage')
             chrome_options.add_argument('--window-size=1920,1080')
         super(StartupBase, self).__init__(
             desired_capabilities=capabilities,
