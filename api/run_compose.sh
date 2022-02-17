c718d03b2bd7c6fa0f10516cb9c736014721732d
Luiz Felipe <lfelipecarvalhedo@gmail.com>
2022-02-17 16:45:08 -0300

    add: tests to docker compose

--- api/run_compose.sh
+++ api/run_compose.sh
@@ -2,10 +2,10 @@
 case $1 in
   "test")
     echo REACT_APP_BASE_URL="backend" > frontend/.env
-    docker-compose --profile test build && docker-compose --profile test up && rm frontend/.env
+    docker-compose --profile test build && docker-compose --profile test up && bash -c rm frontend/.env
     ;;
   *)
     echo REACT_APP_BASE_URL="localhost" > frontend/.env
-    docker-compose build && docker-compose up && rm frontend/.env
+    docker-compose build && docker-compose up && bash -c rm frontend/.env
     ;;
 esac
