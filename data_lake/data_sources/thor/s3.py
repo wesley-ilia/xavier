2557a0f4fda65673f112ea254aaa348cc3186812
gvitor-s <gvitor-s@student.42sp.org.com>
2022-02-21 14:25:50 -0300

    refactory: s3 thor connection

--- data_lake/data_sources/thor/s3.py
+++ data_lake/data_sources/thor/s3.py
@@ -1,44 +1,41 @@
-from os import path
+from os import getenv
 from io import BytesIO
 from pandas import DataFrame
-from configparser import ConfigParser
-from boto3 import setup_default_session, client
+from boto3 import setup_default_session, client, resource
 
 
 class S3:
-    def __init__(self, df: DataFrame):
-        self.credentials = self.__parsing_config()
-        self.client = self.__create_session()
-        self.df = df
+    def __init__(self):
+        self.__create_session()
+        self.client = client('s3')
+        self.resource = resource('s3')
 
-    def __parsing_config(self) -> dict:
-        config = ConfigParser()
-        _path = path.join(path.expanduser('~'), '.aws/credentials')
-        config.read(_path)
-        credentials = {
-                'aws_access_key_id': config.get(
-                    'ilia-ecole42-xavier', 'aws_access_key_id'),
-                'aws_secret_access_key': config.get(
-                    'ilia-ecole42-xavier', 'aws_secret_access_key'),
-                'aws_region': config.get('ilia-ecole42-xavier', 'aws_region')
-                }
-        return credentials
+    def __enter__(self):
+        return self
+
+    def __exit__(self, tp, vl, tb):
+        del self
 
     def __create_session(self):
         setup_default_session(
-                region_name=self.credentials['aws_region'],
-                aws_access_key_id=self.credentials['aws_access_key_id'],
-                aws_secret_access_key=self.credentials['aws_secret_access_key']
+                region_name=getenv('AWS_REGION'),
+                aws_access_key_id=getenv('AWS_ACCESS_KEY_ID'),
+                aws_secret_access_key=getenv('AWS_SECRET_ACCESS_KEY')
                 )
-        return client('s3')
 
-    def send_to_s3(self, bucker_name: str, destination: str) -> int:
+    def send_to_s3(self, bucket_name: str, destination: str, df: DataFrame):
         with BytesIO() as buffer:
-            self.df.to_parquet(buffer, index=False)
-            response = self.client.put_object(
-                    Bucket=bucker_name,
+            df.to_parquet(buffer, index=False)
+            responses = self.client.put_object(
+                    Bucket=bucket_name,
                     Key=destination,
                     Body=buffer.getvalue()
                     )
+        return responses.get("ResponseMetadata", {}).get("HTTPStatusCode")
 
-        return response.get("ResponseMetadata", {}).get("HTTPStatusCode")
+    def download_from_s3(self, bucket_name: str, obj_name: str, name: str):
+        self.client.download_file(
+            bucket_name,
+            obj_name,
+            name
+            )
