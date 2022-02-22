5fe55bd4ceca8a2241106f88f7466dd06b87cc2a
gvitor-s <gvitor-s@student.42sp.org.com>
2022-02-22 14:31:20 -0300

    refactory: make slintel_normalize function

--- data_lake/data_transform/slintel_normalize.py
+++ data_lake/data_transform/slintel_normalize.py
@@ -1,16 +1,12 @@
 import pandas as pd
 
 
-def narray_colunm_to_list(column) -> None:
-    for i in range(len(column)):
-        column[i] = column[i].tolist()
-    return None
+def slintel_normalize():
+    df = pd.read_parquet('./raw_data/slintel.parquet')
 
+    df['name'] = df['name'].str.lower().str.strip()
+    for index, lst in enumerate(df['stacks']):
+        df['stacks'][index] = list(
+                set([string.lower().strip() for string in lst]))
 
-df = pd.read_parquet('./raw_data/slintel.parquet')
-
-df['name'] = df['name'].str.lower().str.strip()
-for index, lst in enumerate(df['stacks']):
-    df['stacks'][index] = list(set([string.lower().strip() for string in lst]))
-
-df.to_parquet("./clean_data/slintel.parquet", index=False)
+    df.to_parquet("./clean_data/slintel.parquet", index=False)
