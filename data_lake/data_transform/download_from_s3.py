30f9d7f0a30cc57ab5b59c00a45b8683968b2fd7
gvitor-s <gvitor-s@student.42sp.org.com>
2022-02-22 14:28:13 -0300

    refactory: use variable from const on download_s3

--- data_lake/data_transform/download_from_s3.py
+++ data_lake/data_transform/download_from_s3.py
@@ -1,12 +1,12 @@
-import os
 from s3 import S3
+from const import OBJ_NAME, BUCKET_NAME
 
 
 def download_file_from_s3(name: str):
     with S3() as s3_obj:
         s3_obj.download_from_s3(
-                bucket_name=os.getenv('BUCKET_NAME'),
-                obj_name=os.getenv('OBJ_NAME') + name,
+                bucket_name=BUCKET_NAME,
+                obj_name=OBJ_NAME + name,
                 name='raw_data/' + name
                 )
 
