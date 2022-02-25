b680d5f2ab5766b2c351eff83c63a702e692be0c
gvitor-s <gvitor-s@student.42sp.org.com>
2022-02-25 14:42:24 -0300

    refactory: remove function loading and create function main

--- data_lake/data_sources/slintel/main.py
+++ data_lake/data_sources/slintel/main.py
@@ -1,6 +1,13 @@
-from utils_get_by_website import loading, delivery
+import concurrent.futures as cf
+from utils_get_by_website import delivery, packaging, loading_info_from_parquet
+
+
+def main():
+    with cf.ProcessPoolExecutor(max_workers=10) as worker:
+        data = worker.map(packaging, loading_info_from_parquet())
+    dt = {name: stacks for name, stacks in data}
+    delivery(dt)
 
 
 if __name__ == "__main__":
-    data = loading()
-    delivery(data)
+    main()
